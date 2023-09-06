4d08ccb6b26dbfe81c77174b66051e421c19e234
TA�ARA FERNANDA SANCHES DIAS <tayara.5970@aluno.pr.senac.br>
2023-09-06 14:33:23 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/teste_teste06/Teste_teste06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste_teste06;
+
+/**
+ *
+ * @author tayara.5970
+ */
+public class Teste_teste06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
